96c85ecc4964e5b31c7632211231304fd212d434
Rafael Difini <rafaeldifini@live.com>
2022-11-21 19:39:44 -0300

    commit final

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -10,9 +10,10 @@ namespace Blog.Data
             : base(options)
         {
         }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Post> Posts { get; set; }
-        public DbSet<User> Users { get; set; }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Post> Posts { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
