89c4b72a0b3a1497e1975dbdc002c39d3e4b63ce
Rafael Difini <rafaeldifini@live.com>
2022-11-23 20:50:26 -0300

    Corrigindo DbContext

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -6,18 +6,18 @@ namespace Blog.Data
 {
     public class BlogDataContext : DbContext
     {
-        public BlogDataContext(DbContextOptions<BlogDataContext> options)
-            : base(options)
-        {
+        // public BlogDataContext(DbContextOptions<BlogDataContext> options)
+        //     : base(options)
+        // {
 
-        }
+        // }
 
         public DbSet<Category> Categories { get; set; } = null!;
         public DbSet<Post> Posts { get; set; } = null!;
         public DbSet<User> Users { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer(@"Server=tcp:blog2812server.database.windows.net,1433;Initial Catalog=blog2812;Persist Security Info=False;User ID=blogadmin;Password=SYNgates-9090;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            => options.UseSqlServer("Server=tcp:blog2812server.database.windows.net,1433;Initial Catalog=blog2812;Persist Security Info=False;User ID=blogadmin;Password=SYNgates-9090;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new CategoryMap());
