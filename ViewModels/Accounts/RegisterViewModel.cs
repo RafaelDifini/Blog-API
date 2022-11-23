972a0a7bc16cb5f977616dc63575d0c43ca031d5
Rafael Difini <rafaeldifini@live.com>
2022-11-23 19:38:28 -0300

    habilitando nulos

--- ViewModels/Accounts/RegisterViewModel.cs
+++ ViewModels/Accounts/RegisterViewModel.cs
@@ -5,9 +5,9 @@ namespace Blog.ViewModels.Accounts;
 public class RegisterViewModel
 {
     [Required(ErrorMessage = "O nome é obrigatório")]
-    public string Name { get; set; }
-    
+    public string? Name { get; set; }
+
     [Required(ErrorMessage = "O E-mail é obrigatório")]
     [EmailAddress(ErrorMessage = "O E-mail é inválido")]
-    public string Email { get; set; }
+    public string? Email { get; set; }
 }
