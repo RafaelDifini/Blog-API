e626762bd8873d3f1861d5a8f87fd8b2e245c58b
Rafael Difini <rafaeldifini@live.com>
2022-11-23 19:48:51 -0300

    teste

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -42,7 +42,8 @@ public class AccountController : ControllerBase
             emailService.Send(user.Name, user.Email, "Bem vindo ao blog!", $"Sua senha é {password}");
             return Ok(new ResultViewModel<dynamic>(new
             {
-                user = user.Email, password
+                user = user.Email,
+                password
             }));
         }
         catch (DbUpdateException)
